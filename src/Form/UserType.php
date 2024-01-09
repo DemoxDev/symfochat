0794cc0dc081c0d369ac4b69f38607bf257a2d95
GabrielC0 <gabriel.changrenier@efrei.net>
2024-01-09 09:40:25 +0100

    [INIT] First commit to init branch

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -9,6 +9,7 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 
+
 class UserType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
