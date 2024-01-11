13ad820618a91c140c901583fb4709c60169ce4c
GabrielC0 <gabriel.changrenier@efrei.net>
2024-01-11 15:11:39 +0100

    [FIX] BUG FIXED

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,32 +2,28 @@
 
 namespace App\Controller;
 
-use App\Service\UserService;
-use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 use App\Entity\User;
 use App\Form\UserType;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Doctrine\ORM\EntityManagerInterface;
+use App\Entity\Message;
+use App\Form\MessageType;
 
 class SecurityController extends AbstractController
 {
     private $entityManager;
 
-    public function __construct(EntityManagerInterface $entityManager, UserService $userService)
+    public function __construct(EntityManagerInterface $entityManager)
     {
         $this->entityManager = $entityManager;
-        $this->userService = $userService;
     }
 
-    #[Route('/logout', name: 'app_logout')]
-    public function logout(): void
-    {
-        throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
-    }
-
+    #[Route(path: '/login', name: 'app_login')]
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
         // if ($this->getUser()) {
@@ -41,21 +37,40 @@ class SecurityController extends AbstractController
 
         return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
     }
+
+    #[Route(path: '/logout', name: 'app_logout')]
+    public function logout(): void
+    {
+        throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
+    }
+
+
     #[Route('/registration', name: 'app_registration', stateless: true)]
-    public function register(Request $request) {
+    public function register(Request $request, UserPasswordHasherInterface $passwordHasher) {
         $user = new User();
         $form = $this->createForm(UserType::class, $user);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-            // Utilisez le service pour enregistrer l'utilisateur
-            $this->userService->registerUser($user);
+            // Encode the password
+            $user->setPassword(
+                $passwordHasher->hashPassword(
+                    $user,
+                    $form->get('plainPassword')->getData()
+                )
+            );
 
-            // Redirigez vers une page de succès ou la page de connexion
+            $user->setIsConnected(false);
+            $user->setLastConnected(new \DateTimeImmutable());
+            // Save the user to the database
+            $this->entityManager->persist($user);
+            $this->entityManager->flush();
+
+            // Redirect to a success page or login page
             return $this->redirectToRoute('app_login');
         }
-
-        // Retourne la réponse
+        
+        // Return response
         return $this->render('security/register.html.twig', [
             'form' => $form->createView()
         ]);
@@ -70,7 +85,7 @@ class SecurityController extends AbstractController
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-            // Associez le message à l'utilisateur actuel      
+            // Associez le message à l'utilisateur actuel
             $message->setUser($this->getUser());
             $message->setCreatedAt(new \DateTimeImmutable());
 
@@ -86,16 +101,6 @@ class SecurityController extends AbstractController
         return $this->render('security/messages.html.twig', [
             'form' => $form->createView(),
             'messages' => $messages,
-        ]);
-    }
-
-    #[Route('/home', name: 'app_home')]
-    public function home(Request $request): Response
-    {
-        $user = $this->getUser();
-    
-        return $this->render('security/home.html.twig', [
-            'user' => $user,
         ]);
     }
 }
